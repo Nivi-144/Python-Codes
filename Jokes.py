bb2b5605c5a7b43ce90b8909e9f647010a8c252e
Nivedita Sen <niveditasen14@gmail.com>
2026-01-13 23:47:28 +0530

    Add files via upload

---
+++ Jokes.py
@@ -0,0 +1,13 @@
+import random
+jokes = [
+    "Life is short. Smile while you still have teeth 😁",
+    "I told my computer I needed a break… now it won’t stop sending me KitKat ads 😂",
+    "Why don’t programmers like nature? Too many bugs 🐛",
+    "I started exercising… turns out running late doesn’t count 🏃‍♂️😅",
+    "My brain has too many tabs open… and one is playing music I can’t find 🔊😂",
+    "I tried to be normal once. Worst two minutes of my life 😐",
+    "Why did the math book look sad? Because it had too many problems 📘😭",
+    "I’m not lazy, I’m just on energy-saving mode 🔋😎"
+]
+print("😂 Random Joke 😂")
+print(random.choice(jokes))
