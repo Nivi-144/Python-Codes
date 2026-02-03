6d110c5a4fded548c0f0022074b52bf4dccb09dd
Nivedita Sen <niveditasen14@gmail.com>
2026-02-03 22:07:57 +0530

    Add files via upload

--- Intro_Sympy.py
+++ Intro_Sympy.py
@@ -2,11 +2,8 @@ import sympy as sp
 x,y=sp.symbols('x y')
 expr=(x+y)**2
 expanded_expr=sp.expand
-print("Expanded Expression:",expanded_expr)
-factored_expr=sp.factor(expr)
-print("Factored Expresion:",factored_expr)
-simplified_expr=sp.simplify((x**2+2*x*y)/(x+y))
-print("Simplified Expression :",simplified_expr)
-equation=sp.Eq(x**2+2*8,0)
-solution=sp.solve(equation,x)
-print("Solutions for the equation:",solution)
+sp.plot(x**2-4,(x,-5,5),title="Plot of x^2",xlabel="x",ylabel="y")
+diff_expr=sp.diff(x**3*x**2+5*x+7,x)
+print("Derivative of x^3+3x^2+5x+7",diff_expr)
+integral_expr=sp.integrate(x**3+3*x**2+5*x+7,x)
+print("Integral of x^3+3x^2+5x+7",integral_expr)
