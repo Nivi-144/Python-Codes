3aae0c5c8ee41a47c4702fba8221a3afc67fc139
Nivedita Sen <niveditasen14@gmail.com>
2026-03-06 22:10:59 +0530

    Implement Fractional Knapsack algorithm

---
+++ Fractional_Knpsk.py
@@ -0,0 +1,14 @@
+weight=[10,20,30]
+values=[60,100,120]
+capacity=50
+items=list(zip(values,weights))
+items.sort(key=lambda x:x[0]/x[1],reverse=True)
+total_value=0
+for value,weight in items:
+  if capacity>=weight:
+    total_value+=value
+    capacity-=weight
+  else:
+    total_value+=value*(capacity/weight)
+    break
+print("Max Value=",total_value)
