da43ec4e91efa954c708e9f5652ac3d24569b908
Nivedita Sen <niveditasen14@gmail.com>
2026-03-13 22:48:20 +0530

    Implement selection sort algorithm in bbb.py

---
+++ bbb.py
@@ -0,0 +1,8 @@
+aar=[7,88,96,42,78]
+for i in range(len(arr)):
+  min=i
+  for j in range(i+1,len(arr)):
+      if arr[j] < arr[min]:
+          min=j
+   arr[i],arr[min]=arr[min],arr[i]
+print("Sorted Array",arr)
