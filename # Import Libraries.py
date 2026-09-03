fdbdfa12175f5846c90754bd149107d05b3e017c
Nivedita Sen <niveditasen14@gmail.com>
2026-09-03 22:04:43 +0530

    Update # Import Libraries.py

--- # Import Libraries.py
+++ # Import Libraries.py
@@ -3,15 +3,18 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
+
 
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.metrics import confusion_matrix, roc_auc_score, roc_curve
@@ -21,16 +24,19 @@ from sklearn.metrics import confusion_matrix, roc_auc_score, roc_curve
 # 1. Load Dataset
 # ==============================
 
+
 data = load_breast_cancer()
+
 
 X = data.data
 y = data.target
-
 print("Total Samples:", X.shape[0])
 print("Total Features:", X.shape[1])
 
+
 print("\nFeature Names:")
 print(data.feature_names)
+
 
 print("\nTarget Classes:")
 print(data.target_names)
@@ -39,6 +45,7 @@ print(data.target_names)
 # ==============================
 # 2. Train Test Split (80:20)
 # ==============================
+
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
@@ -49,7 +56,9 @@ X_train, X_test, y_train, y_test = train_test_split(
 # 3. Feature Scaling
 # ==============================
 
+
 scaler = StandardScaler()
+
 
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
@@ -58,6 +67,7 @@ X_test_scaled = scaler.transform(X_test)
 # ==============================
 # 4. Models
 # ==============================
+
 
 models = {
     "KNN": KNeighborsClassifier(n_neighbors=5),
